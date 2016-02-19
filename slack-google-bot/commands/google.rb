37fabeeccfb4f613a10aed8c8e83a3c8ebe630b7
Jack Steadman <jsteadman@bluestatedigital.com>
2016-02-19 13:02:11 -0500

    bugfix

--- slack-google-bot/commands/google.rb
+++ slack-google-bot/commands/google.rb
@@ -5,7 +5,7 @@ module SlackGoogleBot
         expression = match['expression'].strip
         query = { query: expression }
         query[:cx] = ENV['GOOGLE_CSE_ID'] if ENV['GOOGLE_CSE_ID']
-        result = ::Google::Search::Web.new(query).first
+        result = result ? ::Google::Search::Web.new(query).first : "Didn't find anything. Might have to ask human Seth."
         message = result.title + "\n" + result.uri
         send_message client, data.channel, message
       end
