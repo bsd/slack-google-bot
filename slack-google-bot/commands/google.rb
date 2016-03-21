c687b8290e6d8ca19d93768adb73bd389a4d70ac
Jack Steadman <jsteadman@bluestatedigital.com>
2016-03-21 16:26:38 -0400

    mention

--- slack-google-bot/commands/google.rb
+++ slack-google-bot/commands/google.rb
@@ -6,7 +6,7 @@ module SlackGoogleBot
         query = { query: expression }
         query[:cx] = ENV['GOOGLE_CSE_ID'] if ENV['GOOGLE_CSE_ID']
         result = ::Google::Search::Web.new(query).first
-        message = result ? result.title + "\n" + result.uri : "Hmm, I'm not sure. I don't know everything human <!seth> knows."
+        message = result ? result.title + "\n" + result.uri : "Hmm, I'm not sure. I don't know everything human @seth knows."
         send_message client, data.channel, message
 
       end
